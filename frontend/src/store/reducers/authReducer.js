92da9e0aa353040a8dcb33e50cfdfb02348ea077
d.ts <18373101+dtslab@users.noreply.github.com>
2022-05-10 18:52:26 +0700

    3-17: preparing authentication actions - finish

--- frontend/src/store/reducers/authReducer.js
+++ frontend/src/store/reducers/authReducer.js
@@ -1,4 +1,4 @@
-// import { authActions } from "../actions/authActions";
+import { authActions } from "../actions/authActions";
 
 const initState = {
   userDetails: null,
@@ -6,11 +6,11 @@ const initState = {
 
 const reducer = (state = initState, action) => {
   switch (action.type) {
-    // case authActions.SET_USER_DETAILS:
+    case authActions.SET_USER_DETAILS:
     case "DUMY":
       return {
         ...state,
-        // userDetails: action.userDetails,
+        userDetails: action.userDetails,
       };
     default:
       return state;
