1e28f92a1a87e107b15b7ab8db404ab8878dce4a
d.ts <18373101+dtslab@users.noreply.github.com>
2022-05-10 17:12:47 +0700

    3-12: Creating Redirect Component - finish

--- frontend/src/authPages/LoginPage/LoginPageFooter.js
+++ frontend/src/authPages/LoginPage/LoginPageFooter.js
@@ -1,16 +1,31 @@
 import React from "react";
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import RedirectInfo from "../../shared/components/RedirectInfo";
+import { useNavigate } from "react-router-dom";
 
 const LoginPageFooter = ({ handleLogin, isFormValid }) => {
+  const history = useNavigate();
+
+  const handlePushToRegisterPage = () => {
+    history.push("/register");
+  };
   return (
-    <div>
-      <CustomPrimaryButton
-        label="Log in"
-        additionalStyles={{ marginTop: "30px" }}
-        disabled={!isFormValid}
-        onClick={handleLogin}
+    <>
+      <div>
+        <CustomPrimaryButton
+          label="Log in"
+          additionalStyles={{ marginTop: "30px" }}
+          disabled={!isFormValid}
+          onClick={handleLogin}
+        />
+      </div>
+      <RedirectInfo
+        text="Need an account? "
+        redirectText="Create an account"
+        additionalStyles={{ marginTop: "5px" }}
+        redirectHandler={handlePushToRegisterPage}
       />
-    </div>
+    </>
   );
 };
 
